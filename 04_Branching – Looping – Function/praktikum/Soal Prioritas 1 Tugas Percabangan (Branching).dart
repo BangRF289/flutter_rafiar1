d0fcbd66357d3b6270b27280ed118e78f2dcb4bb
Rafi Ar Rafii <123966343+BangRF289@users.noreply.github.com>
2023-08-24 18:38:57 +0700

    Create Soal Prioritas 1 Tugas Percabangan (Branching).dart

---
+++ 04_Branching – Looping – Function/praktikum/Soal Prioritas 1 Tugas Percabangan (Branching).dart
@@ -0,0 +1,14 @@
+// Soal Prioritas 1 (Nilai 80)
+// Tugas Percabangan (Branching)
+void main() {
+  int nilai = 0;
+  if (nilai > 70) {
+    print("Nilai $nilai adalah A");
+  } else if (nilai > 40) {
+    print("Nilai $nilai adalah B");
+  } else if (nilai > 0) {
+    print("Nilai $nilai adalah C");
+  } else {
+    print(" ");
+  }
+}
