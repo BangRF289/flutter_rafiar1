a42f3590568bae9880ed203881d31e103824b8e3
Rafi Ar Rafii <123966343+BangRF289@users.noreply.github.com>
2023-08-24 19:00:37 +0700

    Update Soal Prioritas 2 Tugas Perulangan (Looping).dart

--- 04_Branching – Looping – Function/praktikum/Soal Prioritas 2 Tugas Perulangan (Looping).dart
+++ 04_Branching – Looping – Function/praktikum/Soal Prioritas 2 Tugas Perulangan (Looping).dart
@@ -1,7 +1,9 @@
 // Soal Prioritas 2
 // Tugas Perulangan (Looping)
+
 import 'dart:io';
 
+// Soal 1 SEGITIGA
 void main() {
   for (var i = 0; i < 9; i++) {
     for (var j = 0; j < 9 - i; j++) {
@@ -13,3 +15,26 @@ void main() {
     stdout.writeln();
   }
 }
+
+
+// Soal 2 JAM PASIR
+// void main() {
+//   for (var i = 0; i < 9; i++) {
+//     for (var j = 0; j < i; j++) {
+//       stdout.write(" ");
+//     }
+//     for (var k = 1; k <= 2 * (9 - i) - 1; k++) {
+//       stdout.write("*");
+//     }
+//     stdout.writeln();
+//   }
+//   for (var i = 2; i < 9; i++) {
+//     for (var j = 0; j < 9 - i; j++) {
+//       stdout.write(" ");
+//     }
+//     for (var k = 1; k <= 2 * i - 1; k++) {
+//       stdout.write("*");
+//     }
+//     stdout.writeln();
+//   }
+// }
