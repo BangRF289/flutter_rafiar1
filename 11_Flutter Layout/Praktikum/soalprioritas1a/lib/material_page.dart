dcb5843c829f6f7dae7510b27b5745d5bb94facc
Rafi Ar Rafii <123966343+BangRF289@users.noreply.github.com>
2023-09-13 09:37:11 +0700

    Submit

---
+++ 11_Flutter Layout/Praktikum/soalprioritas1a/lib/material_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Material App"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            ListTile(title: Text("Home")),
+            ListTile(title: Text("Setting"))
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          "This is Material App",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+      ]),
+    );
+  }
+}
