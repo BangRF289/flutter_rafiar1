f722add8ec4f839e90bcfb0375e47888cbb62f36
Rafi Ar Rafii <123966343+BangRF289@users.noreply.github.com>
2023-11-13 12:52:07 +0700

    commit

---
+++ 20_Introduction REST API – JSON serializationdeserialization/Praktikum/Eksplorasi/Eksplorasi/android/app/src/main/kotlin/com/example/kmkmk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kmkmk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- 20_Introduction REST API – JSON serializationdeserialization/Praktikum/Eksplorasi/eksplor/android/app/src/main/kotlin/com/example/eksplor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.eksplor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
